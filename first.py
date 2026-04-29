d8fa76a2a652d0639e053fc138a6f86db628321c
mostafa12762 <mos014d@gmail.com>
2026-04-29 15:09:23 +0300

    keep only project.py

--- first.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-height = [160, 165, 170, 175, 180, 185]
-weight = [55, 60, 65, 70, 75, 80]
-plt.scatter(height,weight,color='blue',marker='*')
-plt.xlabel('height(cm)')
-plt.ylabel('weight(kg)')
-plt.title("Height vs Weight")
-plt.grid(True)
-plt.show();
--- lineplotmat.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-month=['jan','feb','mar','apr','may','jun']
-weight = [55, 60, 65, 70, 75, 80]
-plt.plot(month,weight,color='blue',marker='o',linestyle='-',linewidth=2)
-plt.xlabel('Month')
-plt.ylabel('Weight (kg)')
-plt.title("Weight Trend")
-plt.grid(True)
-plt.show();
